813f8a9b388ebcbef9847f3bdd89b6eb9873c322
Ankul Sinha <ankulsinha@Ankuls-MacBook-Air.local>
2024-02-02 12:06:20 +0530

    REG_Testnet

--- contracts/Registration.sol
+++ contracts/Registration.sol
@@ -104,272 +104,32 @@ contract Registration is Initializable, OwnableUpgradeable {
         return referrerAddress;
     }
 
-    function getDirectReferralsCount(address user)
-        external
-        view
-        returns (uint256)
-    {
-        return allUsers[user].referrals.length;
-    }
-
-    function excludeAddresses(
-        address[] memory source,
-        address[] memory exclusionList
-    ) internal pure returns (address[] memory) {
-        uint256 sourceLength = source.length;
-        uint256 exclusionLength = exclusionList.length;
-
-        if (exclusionLength == 0) {
-            return source;
-        }
-
-        address[] memory result = new address[](sourceLength);
-        uint256 resultIndex = 0;
-
-        for (uint256 i = 0; i < sourceLength; i++) {
-            bool exclude = false;
-
-            for (uint256 j = 0; j < exclusionLength; j++) {
-                if (source[i] == exclusionList[j]) {
-                    exclude = true;
-                    break;
-                }
-            }
-
-            if (!exclude) {
-                result[resultIndex] = source[i];
-                resultIndex++;
-            }
-        }
-
-        // Resize the result array to the correct length
-        assembly {
-            mstore(result, resultIndex)
-        }
-
-        return result;
-    }
-
-    // Add this function to exclude strings from an array of strings
-    function excludeStrings(
-        string[] memory source,
-        string[] memory exclusionList
-    ) internal pure returns (string[] memory) {
-        uint256 sourceLength = source.length;
-        uint256 exclusionLength = exclusionList.length;
-
-        if (exclusionLength == 0) {
-            return source;
-        }
-
-        string[] memory result = new string[](sourceLength);
-        uint256 resultIndex = 0;
-
-        for (uint256 i = 0; i < sourceLength; i++) {
-            bool exclude = false;
-
-            for (uint256 j = 0; j < exclusionLength; j++) {
-                if (
-                    keccak256(abi.encodePacked(source[i])) ==
-                    keccak256(abi.encodePacked(exclusionList[j]))
-                ) {
-                    exclude = true;
-                    break;
-                }
-            }
-
-            if (!exclude) {
-                result[resultIndex] = source[i];
-                resultIndex++;
-            }
-        }
-
-        // Resize the result array to the correct length
-        assembly {
-            mstore(result, resultIndex)
-        }
-
-        return result;
-    }
-
-    // Modify the getReferralsByLevel function
-    function getReferralsByLevel(address user, uint256 level)
+    function getDirectReferrals(string memory uniqueId)
         public
+        view
         returns (string[] memory)
     {
-        require(level > 0 && level <= 5, "Invalid level");
+        address userAddress = userAddressByUniqueId[uniqueId];
+        require(userAddress != address(0), "User not found");
 
-        // Get referrals at the specified level
-        address[] memory referralAddresses = getReferralsAtLevel(user, level);
-        string[] memory referralUniqueIds = new string[](
-            referralAddresses.length
-        );
+        address[] memory referrals = allUsers[userAddress].referrals;
+        string[] memory referralUniqueIds = new string[](referrals.length);
 
-        // Convert referral addresses to uniqueIds
-        for (uint256 i = 0; i < referralAddresses.length; i++) {
-            referralUniqueIds[i] = allUsers[referralAddresses[i]].uniqueId;
+        for (uint256 i = 0; i < referrals.length; i++) {
+            referralUniqueIds[i] = allUsers[referrals[i]].uniqueId;
         }
-
-        // Eliminate addresses from the previous level
-        if (level > 1) {
-            address[] memory previousLevelReferrals = getReferralsAtLevel(
-                user,
-                level - 1
-            );
-
-            // Convert previous level referral addresses to uniqueIds
-            string[] memory previousLevelUniqueIds = new string[](
-                previousLevelReferrals.length
-            );
-            for (uint256 i = 0; i < previousLevelReferrals.length; i++) {
-                previousLevelUniqueIds[i] = allUsers[previousLevelReferrals[i]]
-                    .uniqueId;
-            }
-
-            // Exclude previous level referrals from the current level referrals
-            referralUniqueIds = excludeStrings(
-                referralUniqueIds,
-                previousLevelUniqueIds
-            );
-        }
-
-        // Clear data for levels lower than or equal to the requested level
-        clearDataForLowerLevels(user, level);
-
-        // Store addresses from the current level for future reference
-        storeAddressesForLevel(user, level, referralAddresses);
 
         return referralUniqueIds;
     }
 
-    function getReferralsByLevelCount(address user, uint256 level)
-        external
-        returns (uint256)
-    {
-        string[] memory abc = getReferralsByLevel(user, level);
-        uint256 xyz = abc.length;
-
-        return xyz;
-    }
-
-    function storeAddressesForLevel(
-        address user,
-        uint256 level,
-        address[] memory addresses
-    ) internal {
-        UserInfo storage userInfo = allUsers[user];
-
-        // Store addresses for the specified level
-        userInfo.levels = addresses;
-    }
-
-    function getReferralsAtLevel(address user, uint256 level)
-        internal
-        view
-        returns (address[] memory)
-    {
-        address[] memory currentLevelReferrals = allUsers[user].referrals;
-
-        for (uint256 i = 1; i < level; i++) {
-            currentLevelReferrals = getNextLevelReferrals(
-                currentLevelReferrals
-            );
-        }
-
-        return currentLevelReferrals;
-    }
-
-    function getNextLevelReferrals(address[] memory users)
-        internal
-        view
-        returns (address[] memory)
-    {
-        address[] memory nextLevelReferrals;
-
-        for (uint256 i = 0; i < users.length; i++) {
-            address[] memory currentLevelReferrals = getDirectReferrals(
-                users[i]
-            );
-
-            nextLevelReferrals = appendToArray(
-                nextLevelReferrals,
-                currentLevelReferrals
-            );
-        }
-
-        return nextLevelReferrals;
-    }
-
-    function getDirectReferrals(address user)
-        internal
-        view
-        returns (address[] memory)
-    {
-        return allUsers[user].referrals;
-    }
-
-    function appendToArray(address[] memory array, address[] memory elements)
-        internal
-        pure
-        returns (address[] memory)
-    {
-        address[] memory newArray = new address[](
-            array.length + elements.length
-        );
-        for (uint256 i = 0; i < array.length; i++) {
-            newArray[i] = array[i];
-        }
-        for (uint256 i = 0; i < elements.length; i++) {
-            newArray[array.length + i] = elements[i];
-        }
-        return newArray;
-    }
-
-    function clearDataForLowerLevels(address user, uint256 level) internal {
-        UserInfo storage userInfo = allUsers[user];
-
-        // Clear data for levels lower than the requested level
-        for (uint256 i = 1; i <= level; i++) {
-            // Check if the user is registered to avoid unnecessary clearing
-            if (userInfo.isRegistered) {
-                userInfo.levels = new address[](0);
-                user = userInfo.referrer;
-                userInfo = allUsers[user];
-            } else {
-                // Break if the referrer is not registered
-                break;
-            }
-        }
-    }
-
-    // Add this function to your existing contract
-
-    function getTotalRegisteredUsersByUniqueId(string memory userUniqueId)
+    function getDirectReferralsCount(string memory uniqueId)
         external
         view
         returns (uint256)
     {
-        address user = userAddressByUniqueId[userUniqueId];
-        require(user != address(0), "User not found");
+        address userAddress = userAddressByUniqueId[uniqueId];
+        require(userAddress != address(0), "User not found");
 
-        uint256 totalCount = getTotalRegisteredUsersRecursive(user);
-
-        return totalCount - 1;
-    }
-
-    function getTotalRegisteredUsersRecursive(address user)
-        internal
-        view
-        returns (uint256)
-    {
-        uint256 totalCount = 1; // Include the current user
-
-        address[] memory referrals = allUsers[user].referrals;
-
-        for (uint256 i = 0; i < referrals.length; i++) {
-            totalCount += getTotalRegisteredUsersRecursive(referrals[i]);
-        }
-
-        return totalCount;
+        return allUsers[userAddress].referrals.length;
     }
 }
